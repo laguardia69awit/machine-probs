584ed6600bd5e06072b066783da2d263a083ff99
Luis Laguardia <111828535+laguardia69awit@users.noreply.github.com>
2023-05-15 09:30:06 +0800

    Create Tao.java

---
+++ Tao.java
@@ -0,0 +1,17 @@
+public class Tao {
+
+    private String pangalan;
+    private int edad;
+    public Tao(String pangalan, int edad){
+    this.pangalan = pangalan;
+    this.edad = edad;
+    }
+
+    public String getName(){
+         return pangalan;
+    }
+    public int getAge(){
+        return edad;
+   }
+
+}
