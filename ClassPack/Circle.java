158b265eef6464a8ebd15396eedbe57b35bfec4f
Luis Laguardia <111828535+laguardia69awit@users.noreply.github.com>
2023-05-15 09:31:15 +0800

    Create Circle.java

---
+++ ClassPack/Circle.java
@@ -0,0 +1,21 @@
+package ClassPack;
+
+public class Circle {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    public double getCircumference() {
+        return 2 * Math.PI * radius;
+    }
+}
